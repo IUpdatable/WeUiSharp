8d9a05d11afe73bfc702883c6709c67cf9bbcc9e
IUpdatable <51181716+IUpdatable@users.noreply.github.com>
2021-08-20 15:15:11 +0800

    调整项目结构 新建Demo项目

---
+++ README.md
@@ -0,0 +1,36 @@
+
+# WeUiSharp
+
+基于 WPF 实现的仿 Windows 桌面版微信 UI 界面库<br>
+An unofficial UI library for Windows WeChat based on WPF implementation
+
+中文 | [English]()
+
+
+## Dependencies
+
+* .NET Framework 4.7.2
+
+## Overview
+
+![Brush](https://raw.githubusercontent.com/HandyOrg/HandyOrgResource/master/HandyControl/Resources/Brush.png)
+
+
+## Getting Started
+
+1. Create a 
+2. NuGet Install `WeUiSharp`
+
+
+
+## Components
+
+* Button
+* CheckBox
+
+
+
+### License
+
+MIT
+
---
+++ Src/WeUiSharp.Demo/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using WeUiSharp.Demo.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace WeUiSharp.Demo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
